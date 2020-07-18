ec41e6c4dcd70dde4f77a3d46ffafb2bc25d3230
ViniciusLisboa07 <63267152+ViniciusLisboa07@users.noreply.github.com>
2020-07-18 18:19:01 -0300

    commit

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const express = require('express');
-const app = express();
-const db = require('./db/connection');
+const express    = require('express');
+const app        = express();
+const db         = require('./db/connection');
+const bodyParser = require('body-parser');
 
 const PORT = 3000;
 
 app.listen(PORT, function(){
     console.log(`O express está rodando na porta ${PORT}`);
 });
+
+// body parser 
+app.use(bodyParser.urlencoded({extended: false}));
 
 // db connection
 db
@@ -21,4 +25,7 @@ db
 // routes
 app.get('/', (req, res) =>{
     res.send("Está funcionando olá");
-})
+});
+
+// jobs routes
+app.use('/jobs', require('./routes/jobs'))
--- routes/jobs.js
+++ routes/jobs.js
@@ -2,10 +2,28 @@ const express = require('express');
 const router  = express.Router();
 const Job     = require('../models/Job');
 
+// rota de teste
+router.get('/test', (req, res) =>{
+    res.send('deu certo'); 
+});
+
 // add job by POST
 
 router.post('/add', (req, res)=>{
 
     let {tittle, salary, company, description, email, new_job} = req.body;
+
+    // insert
+    Job.create({
+        tittle,
+        salary,
+        description,
+        company,
+        email,
+        new_job
+    })
+    .them(() => res.redirect('/')
+    .catch(err => console.log(err)));
 });
 
+module.exports = router
